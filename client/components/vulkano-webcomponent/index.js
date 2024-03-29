3ac1afa1fba69691ef55d6e3dab35bdd38ff1db8
José Daniel Paternina <jose@hellodebrain.com>
2024-03-29 17:32:33 -0500

    [update] node-20 improvements

---
+++ client/components/vulkano-webcomponent/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/prefer-default-export */
+import { html, css, LitElement } from 'lit';
+
+export class VulkanoWebcomponent extends LitElement {
+
+  // createRenderRoot() {
+  //   return this;
+  // }
+
+  static styles = css`p {
+    color: #444;
+    border: 1px solid #999;
+    padding: 5px;
+  }`;
+
+  static properties = {
+    name: { type: String },
+  };
+
+  constructor() {
+    super();
+    this.name = 'native webcomponents';
+  }
+
+  render() {
+    return html`<p>You can use vulkano-webcomponent to have ${this.name}!</p>`;
+  }
+}
+
+customElements.define('vulkano-webcomponent', VulkanoWebcomponent);
